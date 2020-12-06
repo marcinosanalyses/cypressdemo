ffe6e94ce4ff161a72d368081570a953cb719955
marcinos <marcinosanalyses@gmail.com>
2020-12-06 21:15:09 +0100

    Added more filter tests

---
+++ cypress/integration/filter_from_jr_list_spec.ts
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+describe("Checking filters with multiple attributes", () => {
+    beforeEach(() => {
+        cy.intercept("**/api/public/job-requests?*").as("getJobRequests");
+        cy.visit('/'+'job-requests');
+        cy.get('.button').contains('Filters').click();
+        cy.wait("@getJobRequests")
+    });
+    it("Open, Close Filters", function () {
+        cy.get('.button').contains('Close filters')
+        cy.get('.section--border-radius > .section')
+        cy.get('.section--border-radius > .el-flex.-padding-top-md')
+        .should('be.visible');
+        cy.get(':nth-child(2) > .-padding > .-flex-direction-row > .el-header')
+        .should('have.text','Remote work')
+        cy.get('.filters-buttons__wrapper')
+        .should('contain','Close filters')
+        .click();
+    });
+    it("Filter by Skills and Location in List view", function () {
+        cy.intercept("**/api/public/locations/autocomplete?*").as("getLocations");
+        cy.get('.filter-input .el-form-select-next__control')
+        .type('Stockholm'); //Type Locations
+        cy.wait("@getLocations").get('#react-select-2-option-0').click()
+        cy.get('.rc-slider-step').click(10,0)
+        cy.get('.el-form-select-next__placeholder').eq(1)
+        //TO DO 
+        //type Jira, Scrum in skills
+    });
+});
--- cypress/integration/filters_from_home_spec.ts
+++ cypress/integration/filters_from_home_spec.ts
@@ -3,16 +3,22 @@ describe("Checking filters with multiple attributes", () => {
     beforeEach(() => {
         cy.visit('/');
     });
-        it("Add a skill and search ", function () {
-            cy.get('.filter-input [placeholder= "Developer, Manager, ..."]').type('Test Automation Engineer'); //Type skills
+        it("Add a Skill and search ", function () {
+            cy.get('.filter-input [placeholder= "Developer, Manager, ..."]').type('Testing'); //Type skills
+            cy.intercept("**/api/public/job-requests?*").as("getJobRequests");
+            cy.get('.button').contains('Search').click();
+            cy.wait("@getJobRequests"); //wait for Job Requests list
+            cy.get('.el-form-text').should('have.value','Testing'); //check if the skill is available in the filter
+        });
+        it("Add a Skill and Location and search ", function () {
+            cy.get('.filter-input [placeholder= "Developer, Manager, ..."]').type('Jira');
             cy.intercept("**/api/public/locations/autocomplete?*").as("getLocations");
             cy.get('.el-form-select-next__control').type('Stockholm'); //Type Locations
             cy.wait("@getLocations").get('#react-select-2-option-0').click()
-        });
-        it("Add a skill and Location and search ", function () {
-            //TO DO
-        });
-        it("Clear filters ", function () {
-            //TO DO
-        })
+            cy.intercept("**/api/public/job-requests?*").as("getJobRequests");
+            cy.get('.button').contains('Search').click();
+            cy.wait("@getJobRequests");
+            cy.get('.el-form-text').should('have.value','Jira');
+            cy.get('.filter-input').should('have.text','Stockholm'); //Check if location is set
     })
+})
