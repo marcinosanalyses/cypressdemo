0a260e40bf5b4f5e58b03c67ee03a38abb4b93ce
marcinos <marcinosanalyses@gmail.com>
2021-01-04 12:35:49 +0100

    set proper order of tests

--- cypress/integration/filter_from_jr_list_spec.ts
+++ cypress/integration/filter_from_jr_list_spec.ts
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+
 describe("Checking filters with multiple attributes", () => {
     beforeEach(() => {
         cy.intercept("**/api/public/job-requests?*").as("getJobRequests");
